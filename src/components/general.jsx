6a180b452bbd932f1118f4f5a90e4a9567de6f40
Rui Nabeshima <ruinabeshima0321@gmail.com>
2025-10-14 13:30:19 +0900

    Made a form for general information

--- src/components/general.jsx
+++ src/components/general.jsx
@@ -1,8 +1,25 @@
+import "../styles/form.css"
+
 function General() {
   return (
-    <form action="" className="General-Form">
-      <h1></h1>
-    </form>
+    <div className = "container" id="general-container">
+      <form action="" id="general-form" className="form">
+        <label htmlFor="first-name">First Name</label>
+        <input type="text" id="first-name" required/>
+
+        <label htmlFor="middle-name">Middle Name (Optional)</label>
+        <input type="text" id="middle-name" required/>
+
+        <label htmlFor="last-name">Last Name</label>
+        <input type="text" id="last-name"/>
+
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email"/>
+
+        <label htmlFor="phone-number">Phone Number</label>
+        <input type="tel" id="phone-number"/>
+      </form>
+    </div>
   );
 }
 
