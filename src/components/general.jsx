71988f10bc1076a29eb3355ac379061a837f6447
Rui Nabeshima <ruinabeshima0321@gmail.com>
2025-10-14 13:16:52 +0900

    Set up component files

---
+++ src/components/general.jsx
@@ -0,0 +1,9 @@
+function General() {
+  return (
+    <form action="" className="General-Form">
+      <h1></h1>
+    </form>
+  );
+}
+
+export default General;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import General from './components/general.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <General />
   </StrictMode>,
 )
