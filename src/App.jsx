1c7193c866c6364bc666a08e94a97d10bdfbfb11
Rui Nabeshima <ruinabeshima0321@gmail.com>
2025-10-16 12:19:25 +0900

    Implemented all saveState for all forms

--- src/App.jsx
+++ src/App.jsx
@@ -9,37 +9,59 @@ import { useState } from 'react'
 
 function App() {("general")
   const [form, setForm] = useState(1);
-
   const [firstName, setFirstName] = useState("");
   const [middleName, setMiddleName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [number, setNumber] = useState("");
+  const [schoolName, setSchoolName] = useState("")
+  const [schoolStartDate, setSchoolStartDate] = useState("")
+  const [schoolEndDate, setSchoolEndDate] = useState("")
+  const [schoolDescription, setSchoolDescription] = useState("")
 
   const handleFirstName = (event) => {
     setFirstName(event.target.value)
-  }
+  };
 
   const handleMiddleName = (event) => {
     setMiddleName(event.target.value)
-  }
+  };
 
   const handleLastName = (event) => {
     setLastName(event.target.value)
-  }
+  };
 
   const handleEmail = (event) => {
     setEmail(event.target.value)
-  }
+  };
 
   const handleNumber = (event) => {
     setNumber(event.target.value)
-  }
+  };
 
   const nextForm = (event) => {
     event.preventDefault()
     setForm(form + 1);
   };
+
+  const handleSchoolName = (event) => {
+    setSchoolName(event.target.value)
+  };
+
+  const handleSchoolStartDate = (event) => {
+    setSchoolStartDate(event.target.value)
+  };
+
+  const handleSchoolEndDate = (event) => {
+    setSchoolEndDate(event.target.value)
+  };
+
+  const handleSchoolDescription = (event) => {
+    setSchoolDescription(event.target.value)
+  };
+  
+
+
 
   return (
     <>
@@ -54,14 +76,18 @@ function App() {("general")
       {form === 2 && (
         <>
           <General firstName={firstName} middleName = {middleName} lastName = {lastName} email = {email} number = {number} handleFirstName = {handleFirstName} handleMiddleName = {handleMiddleName} handleLastName = {handleLastName} handleEmail = {handleEmail} handleNumber = {handleNumber}/>
-          <Education nextForm = {nextForm} />
+
+          <Education nextForm = {nextForm} schoolName = {schoolName} handleSchoolName = {handleSchoolName} schoolStartDate = {schoolStartDate} handleSchoolStartDate = {handleSchoolStartDate} schoolEndDate = {schoolEndDate} 
+          handleSchoolEndDate = {handleSchoolEndDate} schoolDescription = {schoolDescription} handleSchoolDescription = {handleSchoolDescription} />
           <NextButton nextForm = {nextForm} />
         </>
       )}
       {form === 3 && (
         <>
-          <General />
-          <Education nextForm = {nextForm} />
+          <General firstName={firstName} middleName = {middleName} lastName = {lastName} email = {email} number = {number} handleFirstName = {handleFirstName} handleMiddleName = {handleMiddleName} handleLastName = {handleLastName} handleEmail = {handleEmail} handleNumber = {handleNumber}/>
+          
+          <Education nextForm = {nextForm}  schoolName = {schoolName} handleSchoolName = {handleSchoolName} schoolStartDate = {schoolStartDate} handleSchoolStartDate = {handleSchoolStartDate} schoolEndDate = {schoolEndDate} 
+          handleSchoolEndDate = {handleSchoolEndDate} schoolDescription = {schoolDescription} handleSchoolDescription = {handleSchoolDescription}/>
           <Experience nextForm = {nextForm} />
           <NextButton nextForm = {nextForm} />
         </>
--- src/components/educationform.jsx
+++ src/components/educationform.jsx
@@ -1,30 +1,30 @@
 import "../styles/form.css"
 
-function Education(prop){
+function Education(props){
   return (
     <div className = "container" id="education-container">
       <p class="form-title">EDUCATION</p>
-      <form action="" onSubmit={prop.nextForm} id="education-form" className="form">
+      <form action="" onSubmit={props.nextForm} id="education-form" className="form">
 
         {/* REQUIRED */}
         <div className="input-container">
           <label htmlFor="school-name">Place of Education*</label>
-          <input type="text" id="school-name"/>
+          <input type="text" id="school-name" value={props.schoolName} onChange={props.handleSchoolName}/>
         </div>
 
         <div className="input-container">
           <label htmlFor="education-start-date">Start Date</label>
-          <input type="month" id="education-start-date"/>
+          <input type="month" id="education-start-date" value={props.schoolStartDate} onChange={props.handleSchoolStartDate}/>
         </div>
 
         <div className="input-container">
           <label htmlFor="education-end-date">End Date</label>
-          <input type="month" id="education-end-date"/>
+          <input type="month" id="education-end-date" value={props.schoolEndDate} onChange={props.handleSchoolEndDate}/>
         </div>
 
         <div className="input-container">
           <label htmlFor="school-description">Honors and Awards, Other Information</label>
-          <textarea id="school-description" />
+          <textarea id="school-description" value={props.schoolDescription} onChange={props.handleSchoolDescription}/>
         </div>
       </form>
     </div>
