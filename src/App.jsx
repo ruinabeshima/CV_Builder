6a801c558cd3d7a3f8e031b45fc6b40850866cf9
Rui Nabeshima <ruinabeshima0321@gmail.com>
2025-10-16 12:06:29 +0900

    Added logic to general form for onChange and props

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,32 @@ import { useState } from 'react'
 function App() {("general")
   const [form, setForm] = useState(1);
 
+  const [firstName, setFirstName] = useState("");
+  const [middleName, setMiddleName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+
+  const handleFirstName = (event) => {
+    setFirstName(event.target.value)
+  }
+
+  const handleMiddleName = (event) => {
+    setMiddleName(event.target.value)
+  }
+
+  const handleLastName = (event) => {
+    setLastName(event.target.value)
+  }
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value)
+  }
+
+  const handleNumber = (event) => {
+    setNumber(event.target.value)
+  }
+
   const nextForm = (event) => {
     event.preventDefault()
     setForm(form + 1);
@@ -21,13 +47,13 @@ function App() {("general")
       
       {form === 1 && (
         <>
-          <General nextForm = {nextForm} />
+          <General nextForm = {nextForm} firstName={firstName} middleName = {middleName} lastName = {lastName} email = {email} number = {number} handleFirstName = {handleFirstName} handleMiddleName = {handleMiddleName} handleLastName = {handleLastName} handleEmail = {handleEmail} handleNumber = {handleNumber}/>
           <NextButton nextForm = {nextForm} />
         </>
       )}
       {form === 2 && (
         <>
-          <General />
+          <General firstName={firstName} middleName = {middleName} lastName = {lastName} email = {email} number = {number} handleFirstName = {handleFirstName} handleMiddleName = {handleMiddleName} handleLastName = {handleLastName} handleEmail = {handleEmail} handleNumber = {handleNumber}/>
           <Education nextForm = {nextForm} />
           <NextButton nextForm = {nextForm} />
         </>
--- src/components/generalform.jsx
+++ src/components/generalform.jsx
@@ -1,39 +1,39 @@
 import "../styles/form.css"
 
-function General(prop) {
+function General(props) {
 
   return (
     <div className = "container" id="general-container">
       <p class="form-title">GENERAL</p>
-      <form action="" onSubmit={prop.nextForm} id="general-form" className="form">
+      <form action="" onSubmit={props.nextForm} id="general-form" className="form">
 
         {/* REQUIRED */}
         <div className="input-container">
           <label htmlFor="first-name">First Name*</label>
-          <input type="text" id="first-name"/>
+          <input type="text" id="first-name" value={props.firstName} onChange={props.handleFirstName}/>
         </div>
 
         <div className="input-container">
           <label htmlFor="middle-name">Middle Name</label>
-          <input type="text" id="middle-name"/>
+          <input type="text" id="middle-name" value={props.middleName} onChange={props.handleMiddleName}/>
         </div>
 
         {/* REQUIRED */}
         <div className="input-container">
           <label htmlFor="last-name">Last Name*</label>
-          <input type="text" id="last-name"/>
+          <input type="text" id="last-name" value={props.lastName} onChange={props.handleLastName}/>
         </div>
 
         {/* REQUIRED */}
         <div className="input-container">
           <label htmlFor="email">Email*</label>
-          <input type="email" id="email"/>
+          <input type="email" id="email" value={props.email} onChange={props.handleEmail}/>
         </div>
 
         {/* REQUIRED */}
         <div className="input-container">
           <label htmlFor="phone-number">Phone Number*</label>
-          <input type="tel" id="phone-number"/>
+          <input type="tel" id="phone-number" value={props.number} onChange={props.handleNumber}/>
         </div>
 
         
